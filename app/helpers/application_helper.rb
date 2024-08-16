cf4212b750b033cdafaff5ba56043557ebdd0e48
Pako7 <fco.balam7@gmail.com>
2024-08-15 18:12:38 -0600

    Feat: Responsive Menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def svg_tag(icon_name)
+    file = Rails.root.join("app", "assets", "images", icon_name.to_s).read
+    doc = Nokogiri::HTML::DocumentFragment.parse(file)
+    doc.to_html.html_safe
+  end
 end
